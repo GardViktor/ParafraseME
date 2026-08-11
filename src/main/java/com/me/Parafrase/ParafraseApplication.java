0bba4a27de949c9a51100b3543a94a41de92edb2
Edgard <edgardvictorlol@gmail.com>
2026-08-11 08:23:15 -0300

    chore: estruturar projeto inicial do Parafrase.me

---
+++ src/main/java/com/me/Parafrase/ParafraseApplication.java
@@ -0,0 +1,13 @@
+package com.me.Parafrase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParafraseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParafraseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/Parafrase/ParafraseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.Parafrase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParafraseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
