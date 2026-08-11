7959b61693054d331863d22ad65c10ba75c90209
Edgard <edgardvictorlol@gmail.com>
2026-08-11 15:48:06 -0300

    feat: criar entidade Exercicio

---
+++ src/main/java/com/me/Parafrase/exercicio/Execicio.java
@@ -0,0 +1,26 @@
+package com.me.Parafrase.exercicio;
+
+import com.me.Parafrase.anotacao.Anotacao;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_exercicios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Execicio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String enunciado;
+    private boolean resolvido;
+    @Column(name = "solucao")
+    private String linkSolucao;
+    @ManyToOne
+    @Column(name = "anotacao_id", nullable = false)
+    private Anotacao anotacao;
+}
