0012fa00fbc620636656b41769c398acb116965a
Edgard <edgardvictorlol@gmail.com>
2026-08-15 10:34:02 -0300

    feat: adicionar abstração na camada de service e controller, utilizando AnotacaoDTO e AnotacaoMapper

--- src/main/java/com/me/Parafrase/anotacao/AnotacaoDTO.java
+++ src/main/java/com/me/Parafrase/anotacao/AnotacaoDTO.java
@@ -1,4 +1,25 @@
 package com.me.Parafrase.anotacao;
 
+import com.me.Parafrase.exercicio.Exercicio;
+import com.me.Parafrase.livro.Livro;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnotacaoDTO {
+
+    private Long id;
+    private String capitulo;
+    private Integer pagina;
+    private String minhaAnotacao;
+    private LocalDate dataCadastro;
+    private Livro livros;
+    private List<Exercicio> exercicios;
+
 }
--- src/main/java/com/me/Parafrase/anotacao/AnotacaoMapper.java
+++ src/main/java/com/me/Parafrase/anotacao/AnotacaoMapper.java
@@ -1,4 +1,38 @@
 package com.me.Parafrase.anotacao;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AnotacaoMapper {
+
+    public Anotacao map(AnotacaoDTO anotacaoDTO) {
+
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(anotacaoDTO.getId());
+        anotacao.setCapitulo(anotacao.getCapitulo());
+        anotacao.setPagina(anotacaoDTO.getPagina());
+        anotacao.setMinhaAnotacao(anotacaoDTO.getMinhaAnotacao());
+        anotacao.setDataCadastro(anotacaoDTO.getDataCadastro());
+        anotacao.setLivros(anotacaoDTO.getLivros());
+        anotacao.setExercicios(anotacaoDTO.getExercicios());
+
+        return anotacao;
+
+    }
+
+    public AnotacaoDTO map(Anotacao anotacao) {
+
+        AnotacaoDTO anotacaoDTO = new AnotacaoDTO();
+        anotacaoDTO.setId(anotacao.getId());
+        anotacaoDTO.setCapitulo(anotacao.getCapitulo());
+        anotacaoDTO.setPagina(anotacao.getPagina());
+        anotacaoDTO.setMinhaAnotacao(anotacao.getMinhaAnotacao());
+        anotacaoDTO.setDataCadastro(anotacao.getDataCadastro());
+        anotacaoDTO.setLivros(anotacao.getLivros());
+        anotacaoDTO.setExercicios(anotacao.getExercicios());
+
+        return anotacaoDTO;
+
+    }
+
 }
