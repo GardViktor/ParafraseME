b1c6a2d7d009a9238121fcb053eb8d33cb507dec
Edgard <edgardvictorlol@gmail.com>
2026-08-18 19:51:03 -0300

    feat: adicionar camada de abstração entre o meu model, tendo o acesso somente via dto

--- src/main/java/com/me/Parafrase/exercicio/ExercicioDTO.java
+++ src/main/java/com/me/Parafrase/exercicio/ExercicioDTO.java
@@ -1,4 +1,19 @@
 package com.me.Parafrase.exercicio;
 
+import com.me.Parafrase.anotacao.Anotacao;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ExercicioDTO {
+
+    private Long id;
+    private String enunciado;
+    private boolean resolvido;
+    private String linkSolucao;
+    private Anotacao anotacoes;
+
 }
--- src/main/java/com/me/Parafrase/exercicio/ExercicioMapper.java
+++ src/main/java/com/me/Parafrase/exercicio/ExercicioMapper.java
@@ -1,4 +1,30 @@
 package com.me.Parafrase.exercicio;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ExercicioMapper {
+
+    public Exercicio map(ExercicioDTO exercicioDTO) {
+        Exercicio exercicio = new Exercicio();
+        exercicio.setId(exercicioDTO.getId());
+        exercicio.setEnunciado(exercicioDTO.getEnunciado());
+        exercicio.setResolvido(exercicioDTO.isResolvido());
+        exercicio.setLinkSolucao(exercicioDTO.getLinkSolucao());
+        exercicio.setAnotacoes(exercicioDTO.getAnotacoes());
+
+        return exercicio;
+
+    }
+
+    public ExercicioDTO map(Exercicio exercicio) {
+        ExercicioDTO exercicioDTO = new ExercicioDTO();
+        exercicioDTO.setId(exercicio.getId());
+        exercicioDTO.setEnunciado(exercicio.getEnunciado());
+        exercicioDTO.setResolvido(exercicio.isResolvido());
+        exercicioDTO.setLinkSolucao(exercicio.getLinkSolucao());
+        exercicioDTO.setAnotacoes(exercicio.getAnotacoes());
+
+        return exercicioDTO;
+    }
 }
