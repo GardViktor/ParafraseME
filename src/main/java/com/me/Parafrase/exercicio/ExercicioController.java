f77fb4a2d330ef6d511374d7ea5540e334a82a2d
Edgard <edgardvictorlol@gmail.com>
2026-08-18 19:54:36 -0300

    feat: criar aas funcionalidades para o CRUD

--- src/main/java/com/me/Parafrase/exercicio/ExercicioController.java
+++ src/main/java/com/me/Parafrase/exercicio/ExercicioController.java
@@ -1,4 +1,42 @@
 package com.me.Parafrase.exercicio;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/exercicios")
 public class ExercicioController {
+
+    private ExercicioService exercicioService;
+
+    public ExercicioController(ExercicioService exercicioService) {
+        this.exercicioService = exercicioService;
+    }
+
+    @PostMapping("/criar")
+    public ExercicioDTO criarExercicio(@RequestBody ExercicioDTO exercicioDTO) {
+        return exercicioService.criarExercicio(exercicioDTO);
+
+    }
+
+    @GetMapping("/listar")
+    public List<ExercicioDTO> listarExercicio() {
+        return exercicioService.listarExercicio();
+    }
+
+    @GetMapping("/listar/{id}")
+    public ExercicioDTO listarExercicioID(@PathVariable Long id) {
+        return exercicioService.listarExercicioID(id);
+    }
+
+    @PutMapping("/alterar/{id}")
+    public ExercicioDTO alterarExercicio(@PathVariable Long id, @RequestBody ExercicioDTO exercicioDTO) {
+        return exercicioService.alterarExercicio(id, exercicioDTO);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void deletarExercicio(@PathVariable Long id) {
+        exercicioService.deletarExercicio(id);
+    }
 }
--- src/main/java/com/me/Parafrase/exercicio/ExercicioService.java
+++ src/main/java/com/me/Parafrase/exercicio/ExercicioService.java
@@ -1,4 +1,54 @@
 package com.me.Parafrase.exercicio;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class ExercicioService {
+    
+    private ExercicioRepository exercicioRepository;
+    private ExercicioMapper exercicioMapper;
+
+    public ExercicioService(ExercicioRepository exercicioRepository, ExercicioMapper exercicioMapper) {
+        this.exercicioRepository = exercicioRepository;
+        this.exercicioMapper = exercicioMapper;
+    }
+
+    public ExercicioDTO criarExercicio(ExercicioDTO exercicioDTO) {
+        Exercicio exercicio = exercicioMapper.map(exercicioDTO);
+        exercicio = exercicioRepository.save(exercicio);
+        return exercicioMapper.map(exercicio);
+
+    }
+
+    public List<ExercicioDTO> listarExercicio() {
+        List<Exercicio> exercicios = exercicioRepository.findAll();
+        return  exercicios.stream()
+                .map(exercicioMapper::map)
+                .collect(Collectors.toList());
+
+    }
+
+    public ExercicioDTO listarExercicioID(Long id) {
+        Optional<Exercicio> exercicio = exercicioRepository.findById(id);
+        return exercicio.map(exercicioMapper::map).orElse(null);
+    }
+
+    public ExercicioDTO alterarExercicio(Long id, ExercicioDTO exercicioDTO) {
+        Optional<Exercicio> exercicio = exercicioRepository.findById(id);
+        if (exercicio.isPresent()) {
+            Exercicio exercicioUpdate = exercicioMapper.map(exercicioDTO);
+            exercicioUpdate.setId(id);
+            Exercicio exercicioNew = exercicioRepository.save(exercicioUpdate);
+            return exercicioMapper.map(exercicioNew);
+        }
+        return null;
+    }
+
+    public void deletarExercicio(Long id) {
+        exercicioRepository.deleteById(id);
+    }
 }
