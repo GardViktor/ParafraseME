c08aee1bde1cb0115de5e60f65d6036bce32e7b5
Edgard <edgardvictorlol@gmail.com>
2026-08-26 16:18:52 -0300

    feat: implementar e testar o ResponseEntity da entidade Anotacao

--- src/main/java/com/me/Parafrase/anotacao/AnotacaoController.java
+++ src/main/java/com/me/Parafrase/anotacao/AnotacaoController.java
@@ -1,5 +1,7 @@
 package com.me.Parafrase.anotacao;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,27 +16,48 @@ public class AnotacaoController {
     }
 
     @PostMapping("/criar")
-    public AnotacaoDTO criarAnotacao(@RequestBody AnotacaoDTO anotacao) {
-        return anotacaoService.criarAnotacao(anotacao);
+    public ResponseEntity<String> criarAnotacao(@RequestBody AnotacaoDTO anotacaoDTO) {
+        AnotacaoDTO anotacaoCreate = anotacaoService.criarAnotacao(anotacaoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Anotação ID[" + anotacaoCreate.getId() + "] Criada");
     }
 
     @GetMapping("/listar")
-    public List<AnotacaoDTO> listarAnotacao() {
-        return anotacaoService.listarAnotacao();
+    public ResponseEntity<List<AnotacaoDTO>> listarAnotacao() {
+        List<AnotacaoDTO> anotacoes = anotacaoService.listarAnotacao();
+        return ResponseEntity.ok(anotacoes);
     }
 
     @GetMapping("/listar/{id}")
-    public AnotacaoDTO listarAnotacaoID(@PathVariable Long id) {
-        return anotacaoService.listarAnotacaoID(id);
+    public ResponseEntity<?> listarAnotacaoID(@PathVariable Long id) {
+        AnotacaoDTO anotacaoRead = anotacaoService.listarAnotacaoID(id);
+        if (anotacaoRead != null) {
+            return ResponseEntity.ok(anotacaoRead);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Anotação ID[" + id + "] Não encontrada");
+        }
     }
 
     @PutMapping("/alterar/{id}")
-    public AnotacaoDTO alterarAnotacao(@PathVariable Long id, @RequestBody AnotacaoDTO anotacao) {
-        return anotacaoService.alterarAnotacao(id, anotacao);
+    public ResponseEntity<?> alterarAnotacao(@PathVariable Long id, @RequestBody AnotacaoDTO anotacaoDTO) {
+        AnotacaoDTO anotacaoUpdate = anotacaoService.alterarAnotacao(id, anotacaoDTO);
+        if (anotacaoUpdate != null) {
+            return ResponseEntity.ok(anotacaoUpdate);
+        } else {
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Anotação ID[" + id + "] Não encontrada");
+        }
     }
 
     @DeleteMapping("/deletar/{id}")
-    public void  deletarAnotacao(@PathVariable Long id) {
-        anotacaoService.deletarAnotacao(id);
+    public ResponseEntity<String> deletarAnotacao(@PathVariable Long id) {
+        if (anotacaoService.listarAnotacaoID(id) != null) {
+            anotacaoService.deletarAnotacao(id);
+            return ResponseEntity.ok("Anotação ID[" + id + "] Deletada");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Anotação ID[" + id + "] Não encontrada");
+        }
     }
 }
