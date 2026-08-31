08f8bc22e4b95e034864329b8989bbfe0ae4fb45
Edgard <edgardvictorlol@gmail.com>
2026-08-31 19:39:37 -0300

    fix: corrigir a p metodo de alterar dados, o alterarLivros.html estava alterando  a tabelamas não salvando os dados nela

--- src/main/java/com/me/Parafrase/livro/LivroControllerUI.java
+++ src/main/java/com/me/Parafrase/livro/LivroControllerUI.java
@@ -1,7 +1,5 @@
 package com.me.Parafrase.livro;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
